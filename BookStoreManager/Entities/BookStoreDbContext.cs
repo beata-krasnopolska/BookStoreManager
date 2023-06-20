93d8e8e730cf4070be23d1b96363e64955a5796b
DESKTOP-HS85L92\Lenovo <beata.krasnopolska@interia.pl>
2023-06-20 18:59:44 +0200

    Add BookStoreDbContext

---
+++ BookStoreManager/Entities/BookStoreDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreManager.Entities
+{
+    public class BookStoreDbContext : DbContext
+    {
+        private string _connectionString = "Server=(Localdb)\\mssqllocaldb;Database=BookStoreDb;Trusted_Connection=True;";
+        public DbSet<BookStore> BookStores { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BookStore>().Property(r => r.Name).IsRequired().HasMaxLength(25);
+            modelBuilder.Entity<Address>().Property(r => r.City).IsRequired().HasMaxLength(15);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
