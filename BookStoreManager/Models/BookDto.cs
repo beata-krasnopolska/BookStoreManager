8f77e2502a74ca97a7423fcd87b21d20eeb47a75
DESKTOP-HS85L92\Lenovo <beata.krasnopolska@interia.pl>
2023-06-23 16:40:59 +0200

    Add Dto's

---
+++ BookStoreManager/Models/BookDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStoreManager.Models
+{
+    public class BookDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ BookStoreManager/Models/BookStoreDto.cs
@@ -0,0 +1,18 @@
+﻿using BookStoreManager.Entities;
+using System.Collections.Generic;
+
+namespace BookStoreManager.Models
+{
+    public class BookStoreDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool HasDelivery { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+
+        public List<Book> Books { get; set; }
+    }
+}
