efbc88b1ab5477eebb59e10c97eef1d83eb14d4c
DESKTOP-HS85L92\Lenovo <beata.krasnopolska@interia.pl>
2023-06-30 20:23:39 +0200

    Add UpdateBookStoreDto

---
+++ BookStoreManager/Models/UpdateBookStoreDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreManager.Models
+{
+    public class UpdateBookStoreDto
+    {
+        [Required]
+        [MaxLength(25)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool HasDelivery { get; set; }
+    }
+}
