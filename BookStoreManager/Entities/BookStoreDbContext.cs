aca54d75c423f32185ce970b734062692bfa3a02
DESKTOP-HS85L92\Lenovo <beata.krasnopolska@interia.pl>
2023-07-05 13:05:49 +0200

    Add User and Role entities

--- BookStoreManager/Entities/BookStoreDbContext.cs
+++ BookStoreManager/Entities/BookStoreDbContext.cs
@@ -8,9 +8,13 @@ namespace BookStoreManager.Entities
         public DbSet<BookStore> BookStores { get; set; }
         public DbSet<Book> Books { get; set; }
         public DbSet<Address> Addresses { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<User>().Property(r => r.Email).IsRequired();
+            modelBuilder.Entity<Role>().Property(r => r.Name).IsRequired();
             modelBuilder.Entity<BookStore>().Property(r => r.Name).IsRequired().HasMaxLength(25);
             modelBuilder.Entity<Address>().Property(r => r.City).IsRequired().HasMaxLength(15);
         }
