ece087d87ee45b1fecf22c82bfa594e78ffbe968
DESKTOP-HS85L92\Lenovo <beata.krasnopolska@interia.pl>
2023-07-05 16:00:55 +0200

    Add RegisterUserDto

---
+++ BookStoreManager/Models/RegisterUserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreManager.Models
+{
+    public class RegisterUserDto
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(10)]
+        public string Password { get; set; }
+        public string Nationality { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public int RoleId { get; set; } = 3;
+    }
+}
