1fe0ec0e549d4d05d6fbd725d922927069c5aa51
DESKTOP-HS85L92\Lenovo <beata.krasnopolska@interia.pl>
2023-07-05 16:06:26 +0200

    Add AccountController

---
+++ BookStoreManager/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using BookStoreManager.Models;
+using BookStoreManager.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreManager.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost("register")]
+        public ActionResult RegisterUser([FromBody] RegisterUserDto registerUserDto)
+        {
+            _accountService.RegisterUser(registerUserDto);
+
+            return Ok();
+        }
+    }
+}
